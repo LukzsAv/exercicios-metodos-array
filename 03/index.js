67d95fe48bd2c5cff1e02e7c8753046e420a5db0
Lucas Aguiar <lucas.aguiarv@gmail.com>
2023-09-01 17:32:56 -0300

    03 pronto

---
+++ 03/index.js
@@ -0,0 +1,11 @@
+const numeros = [1, 98, -76, 0, 12, 19, 5, 60, 44]
+
+const maiorN = numeros.reduce((acumulador, elementoAtual, index, array)=>{
+    let maior = acumulador
+    if(elementoAtual > maior){
+        maior = elementoAtual
+    }
+    return maior
+})
+
+console.log(maiorN)
